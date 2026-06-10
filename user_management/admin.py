a1b310bfe1d98f161cfaa2217c7b7a4e74c92d9c
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2026-06-10 14:01:50 +0800

    Initialized project

---
+++ user_management/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+# CustomUser = get_user_model()
+
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = UserAdmin.fieldsets + (
+#         (None, {'fields': ('fullname', 'nickname')}),
+#     )
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         (None, {'fields': ('fullname', 'nickname')}),
+#     )
---
+++ user_management/urls.py
@@ -0,0 +1,6 @@
+"""This file sets up the urls for the user_management app."""
+from django.urls import path
+
+urlpatterns = [
+]
+app_name = 'user_management'
