7e71c7194f780bec8d2fdf82a4c948b575675206
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2026-06-10 15:24:45 +0800

    Added UX to forms

--- DILMDashboard/urls.py
+++ DILMDashboard/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="registration/login.html"), name="login"),
-    path('user/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
