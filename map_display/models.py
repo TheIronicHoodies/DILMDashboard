281fe11ea38c28aad2d033bbe53f723472eda1b7
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2026-06-10 23:52:21 +0800

    Added map to homepage

--- map_display/models.py
+++ map_display/models.py
@@ -2,5 +2,5 @@ from django.db import models
 import folium
 
 # Create your models here.
-map = folium.Map(location=[45.5236, -122.6750], zoom_start=13)
-map.save("templates/map_display/home.html")
+
+    
--- map_display/views.py
+++ map_display/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+import folium
 
 # Create your views here.
+class MapView(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        COORDINATES = [12.8797, 121.7740] # Coordinates to the centre of the Philippines
+    
+        figure = folium.Figure(width=800, height=500)
+        ph_map = folium.Map(
+            COORDINATES,
+            width="50%", # width of the map
+            height="50%", # height of the map
+            zoom_start=12, # the staring zoom
+            tiles="OpenStreetMap", # desired tile for the map respresentation
+            zoom_control=True # controls for zoom level (True by default)
+        )
+        
+        ph_map.add_to(figure)
+
+        figure.render()
+        return {"map": figure}
+    
