4e8f3dfe5ad17026b0beba5b3b0ee7ab8371094a
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2026-06-26 13:10:31 +0800

    just stashing changes

--- map_display/views.py
+++ map_display/views.py
@@ -15,17 +15,11 @@ class MapView(TemplateView):
                 return json.load(f)
             
         COORDINATES = [12.8797, 121.7740] # Coordinates to the centre of the Philippines
-        # PH_BOUNDS = requests.get("https://raw.githubusercontent.com/faeldon/philippines-json-maps/refs/heads/master/2023/geojson/country/medres/country.0.01.json", timeout=10).json()
-        # PH_BOUNDS = {}
-        PH_BOUNDS = get_map("static/map_coordinates/legis_dists.json")
-        # PATH = "static/map_coordinates/"
-    
-        # for file in os.scandir(PATH):
-        #     if file.is_file() and file.name.endswith('.json'):
-        #         data=get_map(file)
-        #         PH_BOUNDS = {**PH_BOUNDS, **data}
+        PH_BOUNDS = [[3,115],[25,135]]
+        PH_COASTLINE = get_map("static/map_coordinates/legis_dists.json")
 
         figure = folium.Figure(width="100%", height="100%") # width and height of the figure that will contain the map
+        
         ph_map = folium.Map(
             COORDINATES,
             width="100%", # width of the map
@@ -33,19 +27,26 @@ class MapView(TemplateView):
             zoom_start=6, # the starting zoom
             tiles=None, # desired tile for the map respresentation
             max_bounds=True, # whether to set max bounds to the map (False by default)
-            max_zoom = 15,
+            max_zoom = 10,
             min_zoom = 5,
+            min_lat=PH_BOUNDS[0][0], 
+            max_lat=PH_BOUNDS[1][0], 
+            min_lon=PH_BOUNDS[0][1], 
+            max_lon=PH_BOUNDS[1][1], 
             zoom_control=False # controls for zoom level (True by default)
         )
 
         folium.Choropleth(
-            geo_data=PH_BOUNDS,
+            geo_data=PH_COASTLINE,
+            fill_color="#0000ff",
             fill_opacity=0.5,
             line_opacity=1,
             line_weight=1,
+            nan_fill_color="white",
+            nan_fill_opacity=1,
         ).add_to(ph_map)
         
-        ph_map.fit_bounds(ph_map.get_bounds())
+        # ph_map.fit_bounds(ph_map.get_bounds())
         ph_map.add_to(figure)
 
         # folium.GeoJson(PH_BOUNDS, name='philippines').add_to(ph_map)
