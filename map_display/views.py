78448f8dafb271327ce6a466142130f6bd6dac62
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2026-06-29 17:23:20 +0800

    Cleaned csv and geojson more; displayed map using folium geojson instead of choropleth

--- map_display/views.py
+++ map_display/views.py
@@ -1,7 +1,9 @@
+from branca.colormap import linear
 from django.views.generic import TemplateView
 import folium
 import json
 import os
+import pandas
 import requests
 
 # Create your views here.
@@ -15,7 +17,8 @@ class MapView(TemplateView):
             
         COORDINATES = [12.8797, 121.7740] # Coordinates to the centre of the Philippines
         PH_BOUNDS = [[3,115],[25,135]]
-        PH_COASTLINE = get_map("static/map_coordinates/legis_dists.json")
+        geo_json_data = get_map("static/map_coordinates/legis_dists.json")
+        csv_data = pandas.read_csv("static/map_coordinates/districts_numbers.csv")
 
         figure = folium.Figure(width="100%", height="100%") # width and height of the figure that will contain the map
         
@@ -28,68 +31,41 @@ class MapView(TemplateView):
             max_bounds=True, # whether to set max bounds to the map (False by default)
             max_zoom = 10,
             min_zoom = 5,
-            min_lat=PH_BOUNDS[0][0], 
-            max_lat=PH_BOUNDS[1][0], 
-            min_lon=PH_BOUNDS[0][1], 
-            max_lon=PH_BOUNDS[1][1], 
+            min_lat=PH_BOUNDS[0][0],
+            max_lat=PH_BOUNDS[1][0],
+            min_lon=PH_BOUNDS[0][1],
+            max_lon=PH_BOUNDS[1][1],
             zoom_control=False # controls for zoom level (True by default)
         )
 
-        folium.Choropleth(
-            geo_data=PH_COASTLINE,
-            fill_color="#0000ff",
-            fill_opacity=0.5,
-            line_opacity=0,
-            line_weight=0,
-            nan_fill_color="white",
-            nan_fill_opacity=1,
+        colormap = linear.YlGn_09.scale(
+            csv_data.collected_signatures.min(),
+            csv_data.collected_signatures.max()
+        )
+
+        csv_data_dict = csv_data.set_index('legis_dist')['collected_signatures']
+
+        folium.features.GeoJson(
+            geo_json_data,
+            style_function=lambda feature: {
+                "fillColor": colormap(csv_data_dict[feature["properties"]["legis_dist"]]),
+                "color": "#0000ff",
+                "weight": 0,
+            },
+            zoom_on_click=True,
         ).add_to(ph_map)
-        
-        # ph_map = folium.Map(
-        #     COORDINATES,
-        #     width="100%", # width of the map
-        #     height="100%", # height of the map
-        #     zoom_start=6, # the starting zoom
-        #     tiles=None, # desired tile for the map respresentation
-        #     max_bounds=True, # whether to set max bounds to the map (False by default)
-        #     max_zoom = 10,
-        #     min_zoom = 5,
-        #     min_lat=PH_BOUNDS[0][0], 
-        #     max_lat=PH_BOUNDS[1][0], 
-        #     min_lon=PH_BOUNDS[0][1], 
-        #     max_lon=PH_BOUNDS[1][1], 
-        #     zoom_control=False # controls for zoom level (True by default)
-        # )
-
-        # features = PH_COASTLINE.get("features", [])
-        # if not features:
-        #     raise ValueError("The GeoJSON file does not contain any features.")
-
-        # missing_property = any(
-        #     not (feature.get("properties") or {}).get("legis_dist")
-        #     for feature in features
-        # )
-        # if missing_property:
-        #     raise ValueError(
-        #         "Each GeoJSON feature must include 'properties.legis_dist' for Choropleth mapping."
-        #     )
 
         # folium.Choropleth(
         #     geo_data=PH_COASTLINE,
-        #     data=data,
-        #     columns=['legis_dist', 'collected_signatures'],
-        #     key_on='feature.properties.legis_dist',
-        #     fill_color="PiYG",
+        #     fill_color="#0000ff",
         #     fill_opacity=0.5,
         #     line_opacity=0,
         #     line_weight=0,
-        #     nan_fill_color="black",
+        #     nan_fill_color="white",
         #     nan_fill_opacity=1,
         # ).add_to(ph_map)
 
-        # # folium.GeoJson(PH_BOUNDS, name='philippines').add_to(ph_map)
-        # # folium.LayerControl().add_to(ph_map)
-
-        # figure.render()
-        # return {"map": figure}
+        ph_map.add_to(figure)
+        figure.render()
+        return {"map": figure}
     
